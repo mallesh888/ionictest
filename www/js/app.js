c030b38fd2ed567465150b6b09e9210355150ccb
ramireddy <ramireddy@anipr.in>
2016-07-10 12:41:23 +0530

    added exaples page

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'ionic-material']);
+var app = angular.module('starter', ['ionic', 'ionic-material', 'ngImgCrop']);
 
 app.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -85,6 +85,16 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             'menuContent': {
                 templateUrl: 'templates/demo.html',
                 controller: 'DemoCtrl'
+            }
+        }
+    })
+
+    .state('app.examples', {
+        url: '/examples',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/examples.html',
+                controller: 'ExamplesCtrl'
             }
         }
     })
---
+++ www/js/controllers/examples.js
@@ -0,0 +1,68 @@
+app.controller('ExamplesCtrl', function($scope) {
+	// start examples crtl
+
+	// ngimage croping code
+	$scope.myImage='';
+    $scope.myCroppedImage='';
+
+    var handleFileSelect=function(evt) {
+      var file=evt.currentTarget.files[0];
+      var reader = new FileReader();
+      reader.onload = function (evt) {
+        $scope.$apply(function($scope){
+          $scope.myImage=evt.target.result;
+        });
+      };
+      reader.readAsDataURL(file);
+    };
+    angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
+
+    // rating code start here
+    $scope.rating = 5;
+    $scope.rateFunction = function(rating) {
+
+    };
+
+})
+.directive('starRating',
+	function() {
+		return {
+			restrict : 'A',
+			template : '<ul class="rating">'
+					 + '	<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
+					 + '\u2605'
+					 + '</li>'
+					 + '</ul>',
+			scope : {
+				ratingValue : '=',
+				max : '=',
+				onRatingSelected : '&'
+			},
+			link : function(scope, elem, attrs) {
+				var updateStars = function() {
+					scope.stars = [];
+					for ( var i = 0; i < scope.max; i++) {
+						scope.stars.push({
+							filled : i < scope.ratingValue
+						});
+					}
+				};
+				
+				scope.toggle = function(index) {
+					scope.ratingValue = index + 1;
+					scope.onRatingSelected({
+						rating : index + 1
+					});
+				};
+				
+				scope.$watch('ratingValue',
+					function(oldVal, newVal) {
+						if (newVal) {
+							updateStars();
+						}
+					}
+				);
+			}
+		};
+	}
+);
